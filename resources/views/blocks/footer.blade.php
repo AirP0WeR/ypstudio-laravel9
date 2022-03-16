1590402edc687c0e957ba1c4d670c667e2354d6c
airp0wer <mgyp@mail.ru>
2022-03-16 21:16:21 +0300

    2022c

--- resources/views/blocks/footer.blade.php
+++ resources/views/blocks/footer.blade.php
@@ -3,6 +3,6 @@
       <p class="float-end mb-1">
         <a href="#">Back to top</a>
       </p>
-      <p class="mb-1">Yulia Perezhogina &copy; 2017-2021</p>
+      <p class="mb-1">Yulia Perezhogina &copy; 2017-2022</p>
     </div>
   </footer>
